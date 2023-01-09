df5ad93c89969b39f21a3230f920e9a3ec2eca13
Vishnu Prasad M <vpm735@gmail.com>
2023-01-09 12:26:27 +0530

    Add files via upload

---
+++ ARRuler/ARRuler/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by qbuser on 09/01/23.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    func addDot(at  hitResult : ARHitTestResult){
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor(named: "red")
+        dotGeometry.materials = [material]
+        let dotNode = SCNNode(geometry: dotGeometry)
+        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        dotNodes.append(dotNode)
+        if dotNodes.count >= 2{
+            calculate()
+        }
+    }
+    func calculate () {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a, 2)+pow(b, 2)+pow(c, 2))
+        updateText(text : "\(distance)", atPosition : end.position)
+    }
+    func updateText(text : String, atPosition : SCNVector3) {
+        textNode.removeFromParentNode()
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+         textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(atPosition.x, atPosition.y, atPosition.z)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+        }
+        dotNodes = [SCNNode]()
+        if let touchlocation = touches.first?.location(in: sceneView){
+            let hitTestResult = sceneView.hitTest(touchlocation, types: .featurePoint)
+            if let hitResult = hitTestResult.first{
+                addDot(at: hitResult)
+            }
+        }
+    }
+}
